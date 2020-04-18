ee04ae7d2c859f12c9bf32127283a7e6be5e00bc
MED <rickyblackwall@gmail.com>
2020-04-18 17:24:06 +0200

    multiple service execution

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import m2tp.serietemp.SerietempApplication;
+import sharedseries.SharedseriesApplication;
+import users.UsersApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SerietempApplication.main(args);
+        UsersApplication.main(args);
+        SharedseriesApplication.main(args);
+    }
+}
