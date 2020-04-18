299fe6942377a5019aa05d38038c5b8a605db660
MED <rickyblackwall@gmail.com>
2020-04-18 16:11:48 +0200

    independant services application class tests

--- src/main/java/sharedseries/SharedseriesApplication.java
+++ src/main/java/sharedseries/SharedseriesApplication.java
@@ -1,4 +1,15 @@
 package sharedseries;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@Configuration
 public class SharedseriesApplication {
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "sharedseries");
+        SpringApplication.run(SharedseriesApplication.class, args);
+    }
 }
